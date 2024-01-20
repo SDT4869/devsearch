f5795d6a639a865e0b9946b371ebab171adc6a23
SDT4869 <srvdutta9114@gmail.com>
2024-01-20 17:44:17 +0530

    changed no. of profiles to view

--- users/views.py
+++ users/views.py
@@ -57,7 +57,7 @@ def registerUser(request):
 
 def profiles(request):
     profiles, search_query = searchProfiles(request)
-    custom_range, profiles = paginateProfiles(request, profiles, 3)
+    custom_range, profiles = paginateProfiles(request, profiles, 6)
     context = {'profiles': profiles, 'search_query': search_query, 'custom_range': custom_range}
     return render(request, 'users/profiles.html', context)
 
@@ -69,7 +69,7 @@ def userProfile(request, pk):
     context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
     return render(request, 'users/user-profile.html', context)
 
-
+    
 @login_required(login_url='login')
 def userAccount(request):
     profile = request.user.profile
